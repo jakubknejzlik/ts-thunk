cc2450f0f46d038948f8d3783ce7f9303e566dca
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-04-04 09:27:03 +0200

    Update build process

--- lib/index copy.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type ThunkFunction<T, U> = (arg: U) => Promise<T> | T;
-export declare type Thunk<T, U = undefined> = T | ThunkFunction<T, U>;
-export declare function isThunkFunction<T, U>(thunk: Thunk<T, U>): thunk is ThunkFunction<T, U>;
-export declare function resolveThunk<T, U>(thunk: Thunk<T, U>, arg?: U): Promise<T> | T;
-export declare function resolveOptionalThunk<T, U>(thunk?: Thunk<T, U>, arg?: U): Promise<T> | T | undefined;
-export declare function resolveAsyncThunk<T, U>(thunk: Thunk<T, U>, arg?: U): T;
-export declare function resolveOptionalAsyncThunk<T, U>(thunk?: Thunk<T, U>, arg?: U): Promise<T | undefined>;
--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type ThunkFunction<T, U> = (arg: U) => T;
-export declare type Thunk<T, U = undefined> = T | ThunkFunction<T, U>;
-export declare type ThunkAsyncFunction<T, U> = (arg: U) => Promise<T>;
-export declare type ThunkAsync<T, U = undefined> = T | ThunkAsyncFunction<T, U>;
-export declare function isThunkFunction<T, U>(thunk: Thunk<T, U>): thunk is ThunkFunction<T, U>;
-export declare function resolveThunk<T, U>(thunk: Thunk<T, U>, arg?: U): Promise<T> | T;
-export declare function resolveOptionalThunk<T, U>(thunk?: Thunk<T, U>, arg?: U): Promise<T> | T | undefined;
-export declare function resolveThunkAsync<T, U>(thunk: Thunk<T, U>, arg?: U): Promise<T>;
-export declare function resolveOptionalThunkAsync<T, U>(thunk?: Thunk<T, U>, arg?: U): Promise<T | undefined>;
